98f2f63173e33b53d86bb7c1cf79270d3f69b4f3
Daniel G <danig@Daniels-MacBook-Pro.local>
2019-10-28 00:16:34 +0000

    task is done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 let joke = require('one-liner-joke');
 
+let availableTags = ['sport', 'IT', 'attitude', 'love'];
+
 app.get('/joke/:number', (req, resp)=> {
     let num = req.params.number;
     let result = '';
@@ -9,6 +11,25 @@ app.get('/joke/:number', (req, resp)=> {
         result += `<h4>${joke.getRandomJoke().body}</h4>***`; 
     }
     resp.send(result);
+})
+
+app.get('/joke/:tag/:number', (req, resp)=> {
+    let tag = req.params.tag;
+    let num = req.params.number;
+    let result = '';
+    if(availableTags.includes(tag)) {
+        for(let i = 0; i < num; i++) {
+            result += `<h4>${joke.getRandomJokeWithTag(tag).body}</h4>***`; 
+        }
+    } else {
+        result = 'No joke for this';
+    }
+
+    resp.send(result);
+})
+
+app.get('*', (req, resp) => {
+    resp.send('<h1>404 - It\'s not funny!</h1>')
 })
 
 app.listen(3000, () => {
